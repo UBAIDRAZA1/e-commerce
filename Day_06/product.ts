73cc86c1d0ad38e564a84433c2f0913b2454a301
Ubaid Raza <141560709+UBAIDRAZA1@users.noreply.github.com>
2025-01-21 23:13:11 +0500

    push

---
+++ Day_06/product.ts
@@ -0,0 +1,22 @@
+export const product = {
+    name: 'product',
+    type: 'document',
+    title: 'Product',
+    fields: [
+        {
+            name: 'productName',
+            type: 'string',
+            title: 'Product Name'
+        },
+        {
+            name: 'price',
+            type: 'number',
+            title: 'Price'
+        },
+        {
+            name: 'image',
+            type: 'image',
+            title: 'Image'
+        },
+    ]
+}
